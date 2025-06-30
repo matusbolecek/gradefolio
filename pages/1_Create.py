3d84423f3fb9a96eb5a22891b4d371249beb25f6
9matt <76255461+9matt@users.noreply.github.com>
2025-06-30 18:41:09 +0200

    small fixes + pd integration

--- pages/1_Create.py
+++ pages/1_Create.py
@@ -2,13 +2,15 @@ import streamlit as st
 import os
 from pathlib import Path
 from openai import OpenAI
+import pandas as pd
+
 import database_manager as dbman
 
 api_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI()
 client.api_key = os.getenv("OPENAI_API_KEY")
 
-@st.dialog("Update succesful") # This has to be fixed
+@st.dialog("Update succesful")
 def successful():
     st.write(f"Inputs have succesfully been added to the database")
     if st.button("Submit"):
@@ -31,25 +33,27 @@ if st.session_state['processed_text'] == None:
 
     if group_name:
         st.subheader('Create an input')
-        choice = st.selectbox('Input type', ['Report', 'Exam', 'Summary', 'Self'])
+        choice = st.selectbox('Input type', ['Report', 'Exam', 'Final', 'Self'])
 
         audio_value = st.audio_input("Record a voice message")
-
         process_button = st.button('Process audio', disabled = not audio_value)
+        st.dataframe(pd.read_csv(f'groups/{group_name}.csv'))
+
         if process_button:
             transcript = client.audio.transcriptions.create(
             model="whisper-1",
             file = audio_value
             )
+            print(transcript.text)
 
             prompt = open(f'prompts/{choice.lower()}.txt', 'r').read()
-
             response = client.responses.create(
                 model="gpt-4.1",
                 input = prompt + transcript.text
             )
 
             st.session_state['processed_text'] = [group_name, choice, response.output_text]
+            print(st.session_state['processed_text'])
             st.rerun()
 else:
     st.header('Processed data:')
@@ -64,11 +68,18 @@ else:
     save_to_db = st.button('Save to database')
     if save_to_db:
         db_path = f'groups/{st.session_state["processed_text"][0]}.db'
+        csv_path = f'groups/{st.session_state["processed_text"][0]}.csv'
+        df = pd.read_csv(csv_path)
 
-        for line in (list(iter(st.session_state['processed_text'][2].splitlines()))):
+        for line in (list(iter(st.session_state['processed_text'][2].splitlines()))):       
             num, comment = line.split('-:-')
-            dbman.add(str(num), st.session_state['processed_text'][1], comment, db_path)
-            successful()
+            dbman.add(str(num), st.session_state['processed_text'][1].upper(), comment, db_path) # Add entry into db
+            df.loc[int(num), f"{st.session_state['processed_text'][1]}s"] += 1 # Entry counter
+        
+        df['Sum'] = df.sum(axis=1, numeric_only=True)
+        df.to_csv(csv_path, index=False)
+
+        successful()
 
     restart = st.button('Restart')
     if restart:
