ab7fa2a5461212d561d811fa099a2e86227b436f
9matt <76255461+9matt@users.noreply.github.com>
2025-07-01 12:05:37 +0200

    error catching and statusbar

--- pages/1_Create.py
+++ pages/1_Create.py
@@ -11,12 +11,16 @@ client = OpenAI()
 client.api_key = os.getenv("OPENAI_API_KEY")
 
 @st.dialog("Update succesful")
-def successful():
-    st.write(f"Inputs have succesfully been added to the database")
+def successful(keyword: str):
+    st.write(f"{keyword} inputs have succesfully been added to the database")
     if st.button("Submit"):
         st.session_state['processed_text'] = None
         st.rerun()
 
+if 'error_msg' not in st.session_state: # init for later error handling
+    st.session_state['error_msg'] = None
+
+# Menu logic
 if 'processed_text' not in st.session_state:
     st.session_state['processed_text'] = None
 
@@ -40,21 +44,41 @@ if st.session_state['processed_text'] == None:
         st.dataframe(pd.read_csv(f'groups/{group_name}.csv'))
 
         if process_button:
-            transcript = client.audio.transcriptions.create(
-            model="whisper-1",
-            file = audio_value
-            )
-            print(transcript.text)
+            with st.sidebar.status('Processing...'):
+                st.write('Transcribing audio...')
+                try:
+                    transcript = client.audio.transcriptions.create(
+                    model="whisper-1",
+                    file = audio_value
+                    )
+                except OpenAIError as e:
+                    st.error(f'Error: {e}')
+                else:
+                    st.write('Processing text...')
+                    prompt = open(f'prompts/{choice.lower()}.txt', 'r').read()
+                    try:
+                        response = client.responses.create(
+                            model="gpt-4.1",
+                            input = prompt + transcript.text
+                        )
+                    except OpenAIError as e:
+                        st.error(f'Error: {e}')
+                    else:
+                        try:
+                            for line in (response.output_text.splitlines()):
+                                num, comment = line.split('-:-')
+                        except:
+                            st.session_state['error_msg'] = (f'An error occurred - Please try again. The AI output is *{response.output_text}*')
+                            st.rerun()
+                        else:
+                            st.session_state['processed_text'] = [group_name, choice, response.output_text]
+                            st.write('Done')
+                            
+                            st.session_state['error_msg'] = None
+                            st.rerun()
 
-            prompt = open(f'prompts/{choice.lower()}.txt', 'r').read()
-            response = client.responses.create(
-                model="gpt-4.1",
-                input = prompt + transcript.text
-            )
-
-            st.session_state['processed_text'] = [group_name, choice, response.output_text]
-            print(st.session_state['processed_text'])
-            st.rerun()
+        if st.session_state['error_msg'] != None:
+            st.error(st.session_state['error_msg'])
 else:
     st.header('Processed data:')
     st.subheader(f'Class: {st.session_state["processed_text"][0]}')
@@ -71,15 +95,35 @@ else:
         csv_path = f'groups/{st.session_state["processed_text"][0]}.csv'
         df = pd.read_csv(csv_path)
 
+        correct_inputs, wrong_inputs = 0, 0
         for line in (list(iter(st.session_state['processed_text'][2].splitlines()))):       
             num, comment = line.split('-:-')
-            dbman.add(str(num), st.session_state['processed_text'][1].upper(), comment, db_path) # Add entry into db
-            df.loc[int(num), f"{st.session_state['processed_text'][1]}s"] += 1 # Entry counter
+            try:
+                num = int(num) # Check if the num is in integer form
+                dbman.add(str(num), st.session_state['processed_text'][1].upper(), comment, db_path) # Add entry into db
+            except ValueError:
+                st.session_state['error_msg'] = ('The input was processed incorrectly. Try again') # These do not show
+                wrong_inputs += 1
+            except:
+                st.session_state['error_msg'] = ('An error occured while saving to the database')
+                wrong_inputs += 1
+            else:
+                df.loc[int(num), f"{st.session_state['processed_text'][1]}s"] += 1 # Entry counter
+                correct_inputs += 1
         
-        df['Sum'] = df.sum(axis=1, numeric_only=True)
-        df.to_csv(csv_path, index=False)
+        if st.session_state['error_msg'] != None:
+            st.error(st.session_state['error_msg'])
+        
+        if correct_inputs > 0:
+            df['Sum'] = df.sum(axis=1, numeric_only=True)
+            df.to_csv(csv_path, index=False)
 
-        successful()
+            if wrong_inputs == 0:
+                successful('All')
+            else:
+                successful(f'{correct_inputs} / {correct_inputs + wrong_inputs}')
+        else:
+            st.error('No correct entries were added to the database!')
 
     restart = st.button('Restart')
     if restart:
