bfa2a21fa248b70cd037772a21d36beec9d7966e
9matt <76255461+9matt@users.noreply.github.com>
2025-07-01 15:26:22 +0200

    sidebar while generating

--- pages/3_Generator.py
+++ pages/3_Generator.py
@@ -12,79 +12,90 @@ api_key = os.getenv('OPENAI_API_KEY')
 client = OpenAI()
 
 def generate(students: list, group_name: str):
+    progressbar_caption = f'Processing {len(students)} file{"s" if len(students) != 1 else ""}'
+    progress, step = 0.0, 1.0 / len(students)
+    progress_placeholder = st.sidebar.empty()
+    progress_placeholder.progress(progress, text=progressbar_caption)
+    
     for sets in students:
         idx, name = sets[0], sets[1]
-
-        # Retrieve entries from database
-        reports, exams, finals, selfs = [], [], [], []
-        final_string = str('')
-        database_path = f'groups/{group_name}.db'
-
-        if os.path.isfile(database_path):
-            results = dbman.search(idx, database_path, "id")
-            for a in results:
-                if a[1] == 'REPORT':
-                    reports.append(a[2])
-                elif a[1] == 'EXAM':
-                    exams.append(a[2])
-                elif a[1] == 'FINAL':
-                    finals.append(a[2])
-                elif a[1] == 'SELF':
-                    selfs.append(a[2])
-
-            if len(reports) != 0:
-                final_string += 'Reports from classes:\n'
-                for x in reports:
-                    final_string += f'{x}\n'
-
-            if len(exams) != 0:
-                final_string += 'Exams:\n'
-                for x in exams:
-                    final_string += f'{x}\n'
-
-            if len(finals) != 0:
-                final_string += 'Final summaries:\n'
-                for x in finals:
-                    final_string += f'{x}\n'
-
-            if len(selfs) != 0:
-                final_string += 'Self evaluations:\n'
-                for x in selfs:
-                    final_string += f'{x}\n'
-
-            # Process with LLM
-            prompt = open('prompts/summary.txt', 'r').read()
-
-            try:
-                response = client.responses.create(
-                    model="gpt-4.1",
-                    input = prompt + final_string
-                )
-            except OpenAIError as e:
-                st.error(f'Error: {e}')        
-
-            print(name)
-            ai_output = response.output_text.replace("**name**", name)
-
-            # Generate PDF
-            tex_filename = 'temp.tex'
-            shutil.copyfile('template.tex', tex_filename)
-
-            with open(tex_filename, "a") as tex_file:
-                tex_file.write(ai_output)
-                tex_file.write('\end{document}')
-
-            pdf, log, _ = PDFLaTeX.from_texfile(tex_filename).create_pdf()
-            pdf_filename = f'exports/{group_name}/{idx}_{name.replace(" ", "-")}.pdf'
-            Path(Path(pdf_filename).parent).mkdir(parents=True, exist_ok=True) # Make sure the output folder exists
+        with st.sidebar.status(f"Processing {name}"):
             
-            with open(pdf_filename, 'wb') as f:
-                f.write(pdf)
-            
-            os.remove('temp.tex')
+            # Retrieve entries from database
+            st.write("Retrieving entries from the database")
+            reports, exams, finals, selfs = [], [], [], []
+            final_string = str('')
+            database_path = f'groups/{group_name}.db'
 
-        else:
-            pass # add error
+            if os.path.isfile(database_path):
+                results = dbman.search(idx, database_path, "id")
+                for a in results:
+                    if a[1] == 'REPORT':
+                        reports.append(a[2])
+                    elif a[1] == 'EXAM':
+                        exams.append(a[2])
+                    elif a[1] == 'FINAL':
+                        finals.append(a[2])
+                    elif a[1] == 'SELF':
+                        selfs.append(a[2])
+
+                if len(reports) != 0:
+                    final_string += 'Reports from classes:\n'
+                    for x in reports:
+                        final_string += f'{x}\n'
+
+                if len(exams) != 0:
+                    final_string += 'Exams:\n'
+                    for x in exams:
+                        final_string += f'{x}\n'
+
+                if len(finals) != 0:
+                    final_string += 'Final summaries:\n'
+                    for x in finals:
+                        final_string += f'{x}\n'
+
+                if len(selfs) != 0:
+                    final_string += 'Self evaluations:\n'
+                    for x in selfs:
+                        final_string += f'{x}\n'
+
+                # Process with LLM
+                st.write("Talking to AI")
+                prompt = open('prompts/summary.txt', 'r').read()
+
+                try:
+                    response = client.responses.create(
+                        model="gpt-4.1",
+                        input = prompt + final_string
+                    )
+                except OpenAIError as e:
+                    st.error(f'Error: {e}')        
+
+                print(name)
+                ai_output = response.output_text.replace("**name**", name)
+
+                # Generate PDF
+                st.write("Generating the PDF")
+                tex_filename = 'temp.tex'
+                shutil.copyfile('template.tex', tex_filename)
+
+                with open(tex_filename, "a") as tex_file:
+                    tex_file.write(ai_output)
+                    tex_file.write('\end{document}')
+
+                pdf, log, _ = PDFLaTeX.from_texfile(tex_filename).create_pdf()
+                pdf_filename = f'exports/{group_name}/{idx}_{name.replace(" ", "-")}.pdf'
+                Path(Path(pdf_filename).parent).mkdir(parents=True, exist_ok=True) # Make sure the output folder exists
+                
+                with open(pdf_filename, 'wb') as f:
+                    f.write(pdf)
+                
+                os.remove('temp.tex')
+                progress += step
+                progress_placeholder.progress(progress, text=progressbar_caption)
+
+            else:
+                st.error(f'A database error occurred - the database *{group_name}.db* could not be found') # this should not happen
 
 st.write("# Generator")
 
